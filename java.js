e285e147be27e7572179143d78f711b6190241aa
rethinkzero <93017797+rethinkzero@users.noreply.github.com>
2025-04-09 12:56:35 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,11 @@
+const MCDonaldDiv = document.getElementById('MCDonald');
+const TrumpDiv = document.getElementById('Trump');
+const InfoDiv = document.getElementById('Info');
 
+MCDonaldDiv.addEventListener('click', function() {
+  infoDiv.innerHTML = 'This Is MCDONALD';
+});
+
+TrumpDiv.addEventListener('click', function() {
+  infoDiv.innerHTML = 'Trump Working on MCDONALD? WHAAAAAA';
+});
