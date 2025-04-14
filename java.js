5e2550dcef2e2fc76ee70e4156c9e721cf0dd48f
rethinkzero <93017797+rethinkzero@users.noreply.github.com>
2025-04-14 12:38:23 -0400

    Update java.js

--- java.js
+++ java.js
@@ -10,3 +10,39 @@ MCDonaldDiv.addEventListener('click', function() {
 TrumpDiv.addEventListener('click', function() {
   infoDiv.innerHTML = 'Trump Working on MCDONALD? WHAAAAAA';
 });
+//work from thursday here
+
+
+
+//work from thursday here
+
+
+const container = document.getElementById("list-container");
+
+for (let i = 1; i <= 5; i++) {
+  const p = document.createElement("p");
+  p.textContent = "This is item #" + i;
+  container.appendChild(p);
+}
+
+let j = 1;
+while (j <= 5) {
+  const p = document.createElement("p");
+  p.textContent = "While loop item #" + j;
+  container.appendChild(p);
+  j++;
+}
+
+
+for (let i = 1; i <= 20; i++) {
+  const p = document.createElement("p");
+  p.textContent = i;
+
+  if (i % 2 !==0) {
+    p.style.color = "red";
+  } else {
+    p.style.color = "blue";
+  }
+
+  container.appendChild(p);
+}
