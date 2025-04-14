00988658e0d1236f63ba31d2652f067ad17dd34c
rethinkzero <93017797+rethinkzero@users.noreply.github.com>
2025-04-14 12:43:47 -0400

    Update java.js

--- java.js
+++ java.js
@@ -38,11 +38,12 @@ for (let i = 1; i <= 20; i++) {
   const p = document.createElement("p");
   p.textContent = i;
 
-  if (i % 2 !==0) {
-    p.style.color = "red";
-  } else {
+  if (i = 2) {
     p.style.color = "blue";
+  } else {
+    p.style.color = "red";
   }
 
   container.appendChild(p);
 }
+
